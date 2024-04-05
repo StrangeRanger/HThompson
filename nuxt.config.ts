770e23b14fd57e09ebc98849f8cc1e2c692f95ad
Hunter T. <strangeranger@hthompson.dev>
2024-04-05 08:28:58 -0700

    Add new css and update style and look of project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,4 +22,7 @@ export default defineNuxtConfig({
       },
     },
   },
+  css: [
+    "./assets/css/main.css",
+  ]
 })
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,10 +3,29 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi
+      }
+    },
+    theme: {
+      defaultTheme: 'dark',
+      themes: {
+        dark: {
+          colors: {}
+        }
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
