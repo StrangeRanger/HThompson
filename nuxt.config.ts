034775e89e20a7ed827ea835a9fac50761f72e80
Hunter T. <strangeranger@hthompson.dev>
2024-04-05 10:15:21 -0700

    Remove unused import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import type { ViteConfig } from '@nuxt/schema'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
