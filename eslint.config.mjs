823dd47325da37441ae7fc084d2612fd6f14bdcb
Hunter T. <strangeranger@hthompson.dev>
2024-08-18 21:51:22 -0700

    Slight eslint config update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
+// @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
   rules: {
     "vue/html-self-closing": [
-      "error",
+      "warn",
       {
         html: {
           void: "always", // Disable 'disallow self-closing on void elements' rule.
