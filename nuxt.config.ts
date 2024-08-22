9a4d820fdd34e53225694bdfb26fa5b0ae10000f
Hunter T. <strangeranger@hthompson.dev>
2024-08-22 16:59:02 -0700

    Update nuxt configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
+  plugins: [
+    process.env.NODE_ENV !== "development" ? "plugins/production/vue-matomo.client.js" : "",
+    process.env.NODE_ENV !== "development" ? "plugins/production/cloudflare.js" : "",
+  ].filter(Boolean),
   nitro: {
     prerender: {
       autoSubfolderIndex: false
@@ -14,51 +18,38 @@ export default defineNuxtConfig({
   modules: [
     "@nuxt/eslint",
     "nuxt-security",
+    "@nuxt/devtools",
     (_options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error - Error exception specified in the Vuetify installation guide...
         config.plugins.push(vuetify({ autoImport: true }));
       });
     },
   ],
   security: {
+    ssg: {
+      meta: true, // Enables CSP as a meta tag in SSG mode
+      hashScripts: true, // Enables CSP hash support for scripts in SSG mode
+      hashStyles: false, // Disables CSP hash support for styles in SSG mode (recommended)
+      exportToPresets: true // Export security headers to Nitro presets
+    },
+    sri: true,
     headers: {
-      crossOriginEmbedderPolicy: 'unsafe-none',
+      crossOriginEmbedderPolicy: process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'require-corp',
       contentSecurityPolicy: {
         "default-src": ["'self'", "https://analytics.hthompson.dev"],
+        "img-src": ["'self'", "blob:"],
+        "script-src-attr": ["'none'"],
+        "style-src": ["'self'", "https:", "'unsafe-inline'"],
         "script-src": [
           "'self'",
           "https:",
-          "'unsafe-inline'",
+          "strict-dynamic",
           "https://analytics.hthompson.dev",
           "https://files.hthompson.dev/scripts/tracking.js",
           "https://static.cloudflareinsights.com",
-          "'nonce-{{nonce}}'",
-          "'strict-dynamic'",
-          "'unsafe-eval'",
         ],
-        "style-src": ["'self'", "'unsafe-inline'"],
-        "img-src": ["'self'", "blob:"],
-        "base-uri": ["'none'"],
-        "object-src": ["'none'"],
-        "upgrade-insecure-requests": true
       },
-      permissionsPolicy: {
-        "camera": [],
-        "display-capture": [],
-        "fullscreen": [],
-        "geolocation": [],
-        "microphone": [],
-        "web-share": [],
-      },
-      referrerPolicy: "strict-origin",
-      strictTransportSecurity: {
-        maxAge: 31536000,
-        includeSubdomains: true,
-        preload: true,
-      },
-      xContentTypeOptions: "nosniff",
-      xFrameOptions: "SAMEORIGIN",
-      xXSSProtection: "1; mode=block",
     },
   },
   vite: {
