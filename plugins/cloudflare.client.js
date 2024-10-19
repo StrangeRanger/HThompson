ef42d738d3df2697fb9c65d98280f0962ca7a995
Hunter T. <strangeranger@hthompson.dev>
2024-10-19 14:03:45 -0700

    Simplify plugin mode

---
+++ plugins/cloudflare.client.js
@@ -0,0 +1,13 @@
+export default defineNuxtPlugin(() => {
+  if (process.env.NODE_ENV !== "development") {
+    useHead({
+      script: [
+        {
+          src: "/cdn-cgi/challenge-platform/scripts/jsd/main.js",
+          crossorigin: "anonymous",
+          referrerpolicy: "origin",
+        },
+      ],
+    });
+  }
+});
