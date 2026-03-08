2bd1b65519a528168761f2ccc6389b8d99ad4dbf
Hunter T. <strangeranger@hthompson.dev>
2026-03-08 15:49:59 -0700

    Won't return cloudflare script if nonce doesn't exist

--- app/layout/cloudflare-rum.tsx
+++ app/layout/cloudflare-rum.tsx
@@ -5,11 +5,11 @@ const CF_BEACON_TOKEN = "9b19e46d62334121a6ddd4133f232767";
 
 interface CloudflareRumProps {
   enabled: boolean;
-  nonce: string | undefined;
+  nonce?: string;
 }
 
 export function CloudflareRum({ enabled, nonce }: CloudflareRumProps) {
-  if (!enabled) return null;
+  if (!enabled || !nonce) return null
 
   return (
     <Script
