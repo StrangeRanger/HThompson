07e451ecf1155db3555ecaadb19e53eb43a8397c
Hunter T. <strangeranger@hthompson.dev>
2026-03-08 14:02:50 -0700

    Add TODO

--- app/layout/cloudflare-rum.tsx
+++ app/layout/cloudflare-rum.tsx
@@ -3,6 +3,7 @@
 import Script from "next/script";
 import { useCspNonce } from "@/app/components/csp-nonce-context";
 
+// TODO: Move to environment variables?
 const CF_BEACON_TOKEN = "9b19e46d62334121a6ddd4133f232767";
 
 export function CloudflareRum({ enabled }: { enabled: boolean }) {
